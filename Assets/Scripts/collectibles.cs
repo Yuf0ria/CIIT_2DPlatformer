9fcf39e6ae20abaf9301ff232ac5d9427b146825
Dani F <114934334+Yuf0ria@users.noreply.github.com>
2023-07-23 19:55:19 +0800

    ;

--- Assets/Scripts/collectibles.cs
+++ Assets/Scripts/collectibles.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class collectibles : MonoBehaviour
 {
+    public Animator anim;
+    public PlayerMovement player;
+    private Vector2 movementInput;
+    public float Speed;
+    public int health;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+
     }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Collectibles"))
+        {
+            Destroy(collision.gameObject);
+            player.moveSpeed += Speed;
+
+
+        }
+
+        if (collision.gameObject.CompareTag("health"))
+        {
+            Destroy(collision.gameObject);
+            player.healthPoints += health;
+        }
+    }
+
+
 }
